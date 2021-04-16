1e5d3e8bdce7126b2959b6d2ebdf6d0fcfd5c0fb
Rachel Lin <rachlin232@gmail.com>
2021-04-16 10:33:23 -0400

    fix db connection

--- api/db/index.js
+++ api/db/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 // general query or select
