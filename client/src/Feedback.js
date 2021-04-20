15d400a6239b01c55fe3e3267b5114863a352b51
Rachel Lin <rachlin232@gmail.com>
2021-04-20 09:19:12 -0400

    update feedback about api loading

--- client/src/Feedback.js
+++ client/src/Feedback.js
@@ -8,13 +8,23 @@ export class Feedback extends React.Component {
                 <h3>pending changes</h3>
                 <ul>
                     <li>fix for long names</li>
+
                     <li>word count limit or scrollbars in profiles</li>
-                    <li>better hosting service? this has been randomly crashing
-                        (I've made changes to the way I deployed the API but we'll see if it changes anything.
-                        If you get errors, please let me know!)
+
+                    <li><del>fix API crashing</del>
+                        <ul>
+                            <li>
+                                I've made changes to the way I deployed the API and it hasn't been crashing for me but
+                                let me know if it does for you. If the page takes a while to load, that likely means that
+                                the page hasn't been visited for a while, so Heroku (where I'm hosting my API) unloaded my 
+                                thing from its servers. If the page loads for long enough that it's an issue, the home page will say so. 
+                                (In other words, as long as it says "loading..." it's probably just taking a while to reload
+                                everything)
+                            </li>
+                        </ul>
                     </li>
-                    <li>text input for "about you" and "looking for" to allow editing in previous lines
-                    </li>
+
+                    <li>text input for "about you" and "looking for" to allow editing in previous lines</li>
                 </ul>
             </div>
         )
